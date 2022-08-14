7e8e4551fbd07e7a3f92219250af5a786b1cbc68
fila90 <ivan.filipovic.fila@gmail.com>
2022-08-14 19:53:14 +0200

    fix labes

--- src/lib/WeatherForecastChart.svelte
+++ src/lib/WeatherForecastChart.svelte
@@ -36,16 +36,18 @@
 					{
 						backgroundColor: 'rgba(173,179,188,0.4)',
 						borderColor: 'rgba(173,179,188,0.4)',
-						borderDash: [1, 7],
+						borderDash: [1, 5],
 						borderWidth: 1,
 						data: weatherYesterday.hour.map(hour => hour[$activeWeatherParam]),
-						label: 'Yesterday'
+						label: 'Yesterday',
+						tension: 0.1
 					},
 					{
 						backgroundColor: 'rgba(250,38,160,1)',
 						borderColor: 'rgba(250,38,160,1)',
 						data: weatherToday.hour.map(hour => hour[$activeWeatherParam]),
-						label: 'Today'
+						label: 'Today',
+						tension: 0.1
 					},
 					{
 						backgroundColor: 'rgba(47,243,224,0.5)',
@@ -53,9 +55,34 @@
 						borderDash: [1, 3],
 						borderWidth: 2,
 						data: weatherTomorrow.hour.map(hour => hour[$activeWeatherParam]),
-						label: 'Tomorrow'
+						label: 'Tomorrow',
+						tension: 0.1
 					}
 				]
+			},
+			options: {
+				plugins: {
+					tooltip: {
+						callbacks: {
+							title: ([context]) =>
+								`${context.dataset.label} @ ${context.label}`,
+							label: context => {
+								switch ($activeWeatherParam) {
+									case 'temp_c':
+										return `${context.formattedValue}°C`
+									case 'pressure_mb':
+										return `${context.formattedValue}°`
+									case 'wind_kph':
+										return `${context.formattedValue}/kph`
+									case 'precip_mm':
+										return `${context.formattedValue} mm`
+									default:
+										return context.raw ? 'Yes' : 'No'
+								}
+							}
+						}
+					}
+				}
 			}
 		})
 	}
